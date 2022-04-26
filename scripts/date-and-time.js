0b20c5c17a6bd90c58acc82030b029f816ee6d4d
agipare <agipareemmanuel5909@gmail.com>
2022-04-26 10:55:50 +0000

    main

---
+++ scripts/date-and-time.js
@@ -0,0 +1,8 @@
+const date = document.querySelector('#dateTime');
+try {
+    const currentTime = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    date.textContent = new Date().toLocaleDateString('en-UK', currentTime);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
+   
