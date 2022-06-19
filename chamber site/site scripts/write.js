6165bd04722cc5ce3e66c561b7f79edbeec09afd
agipare <agipareemmanuel5909@gmail.com>
2022-06-19 01:07:09 +0000

    yeb

--- chamber site/site scripts/write.js
+++ chamber site/site scripts/write.js
@@ -43,4 +43,57 @@ close.onclick = toggleMenu;
 
 // Weather 
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+  {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+
+
+  //directory
+  const requestURL = 'chamber site/directory.json';
+  const cards = document.querySelector('cards');
+  
+  fetch(requestURL)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (jsonObject) {
+          console.table(jsonObject);
+          const prophets = jsonObject['companies'];
+          prophets.forEach(displayProphets);
+      });
+  
+  function displayProphets(prophet) {
+      // Create elements to add to the document
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let birthDate = document.createElement('p');
+      let birthPlace = document.createElement('p');
+      let portrait = document.createElement('img');
+  
+      // Change the textContent property of the h2 element to contain the prophet's full name
+      h2.textContent = `${companies.name} `;
+  
+      // Change the textContent property of the p element to contain the birth date and birth place 
+      birthDate.textContent = `Date of Birth - ${prophet.birthdate}`
+      birthPlace.textContent= `Place of Birth - ${prophet.birthplace}`
+  
+      // Build the images attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropiate variable).
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+      portrait.setAttribute('loading', 'lazy');
+  
+      //  Add/append the section(card) with the h2 element
+      card.appendChild(h2);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+      card.appendChild(portrait);
+  
+      // Add/append the existing HTML div with the cards class with the section(card)
+      document.querySelector('div.cards').appendChild(card);
+  
+  };
+  
+  
+  
+  
